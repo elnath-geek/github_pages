f26550253f258fbf0453ebf41d8c113ea65845e1
elnath-geek <elnath-geek@users.noreply.github.com>
2021-07-09 04:36:13 +0000

    deploy: cb6ed2843309f405b6cb8309dae1ec0de8c4dd2f

--- _nuxt/static/1625804820/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fgithub_pages\u002F_nuxt\u002Fstatic\u002F1625804820",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fgithub_pages\u002F",assetsPath:"\u002Fgithub_pages\u002F_nuxt\u002F",cdnURL:a}}}}(null));
