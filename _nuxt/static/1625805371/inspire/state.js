520a837ce20859b75847deaceb9c860eb44ce522
elnath-geek <elnath-geek@users.noreply.github.com>
2021-07-09 04:43:55 +0000

    deploy: cb7a768394880147a48da75ee4f572f71d04008e

--- _nuxt/static/1625805371/inspire/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fgithub_pages\u002F_nuxt\u002Fstatic\u002F1625805371",layout:"default",error:a,serverRendered:true,routePath:"\u002Finspire",config:{_app:{basePath:"\u002Fgithub_pages\u002F",assetsPath:"\u002Fgithub_pages\u002F_nuxt\u002F",cdnURL:a}}}}(null));
